6e51274fad11585aae0fc2aa27408b6d539c3f5a
Oluwasomidotun <Oluwasomidotun7@gmail.com>
2022-02-17 18:56:19 +0100

    add google search

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -12,7 +12,7 @@ class MyDocument extends Document {
         <Head>
           <meta
             name="google-site-verification"
-            content="H_z2XB6GRiF6i4qSWWOT_qhGNClbSZAHbst5Xc9cy4I"
+            content="qUVimM5RSamX3nLxgiJZY3jbiTdZT7ipQ6-3DVThn90"
           />
           <link
             href="https://fonts.googleapis.com/css2?family=Oswald:wght@300;400;700&display=swap"
