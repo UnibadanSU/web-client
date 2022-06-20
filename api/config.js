bd3d783c95e5e5c92f8e0b3e3397daf0e7b15949
Beke Ijobra <ijobra20@gmail.com>
2022-06-20 01:00:59 +0100

    original config tokens restored

--- api/config.js
+++ api/config.js
@@ -1,26 +1,17 @@
-// import axios from "axios";
-
-// const baseURL = 
-//   process.env.NODE_ENV === "development"
-//     ? process.env.NEXT_PUBLIC_DEV_BASE_API_URL
-//     : process.env.NEXT_PUBLIC_BASE_API_URL;
-
-// const authToken =
-//   process.env.NODE_ENV === "development"
-//     ? process.env.NEXT_PUBLIC_DEV_AUTH_TOKEN
-//     : process.env.NEXT_PUBLIC_AUTH_TOKEN;
-
-// export const requestInstance = axios.create({
-//   baseURL,
-//   headers: {'Authorization': `Bearer ${authToken}`}
-// });
 import axios from "axios";
 
-const baseURL = 'https://uisu-api.herokuapp.com/api'
+const baseURL = 
+  process.env.NODE_ENV === "development"
+    ? process.env.NEXT_PUBLIC_DEV_BASE_API_URL
+    : process.env.NEXT_PUBLIC_BASE_API_URL;
 
-const authToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNjU1NTQ2NDA4LCJleHAiOjE2NTgxMzg0MDh9.nCTdjY1LaGmwYusnyQmRxGV9q19owT6XTh8rUctFBCg'
+const authToken =
+  process.env.NODE_ENV === "development"
+    ? process.env.NEXT_PUBLIC_DEV_AUTH_TOKEN
+    : process.env.NEXT_PUBLIC_AUTH_TOKEN;
 
 export const requestInstance = axios.create({
   baseURL,
   headers: {'Authorization': `Bearer ${authToken}`}
 });
+
