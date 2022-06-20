68ebc7ca27c1860011f8d7e3628151265db44e7c
evidencebeke <ijobra20@gmail.com>
2022-06-20 03:46:47 +0100

    prepared config for su profile pages

--- api/executives.js
+++ api/executives.js
@@ -6,3 +6,13 @@ export const getExecutives = async () => {
     return response.data;
   } catch (error) {}
 };
+export const getExecutive = async (id) => {
+  try {
+    const response = await requestInstance.get(
+      `/executives?filters[id][$eq]=${id}&populate=%2A`
+    );
+    return response.data;
+  } catch {
+    return null;
+  }
+};
